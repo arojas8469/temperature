ecc588647dcff37619354bde2aced9d54a847731
Anthony Rojas <anthony.rojas099@gmail.com>
2024-12-12 11:41:04 -0500

    Intial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//1. Declare Variables
+// Usimg let
+let character = "Angel";
+let action = "ate";
+let object = "Pizza";
+let place = "Brookyln";
+let time = "a rainy afternoon";
+
+// using Const
+
+const occasion = "get together";
+
+// 2. Create Story
+console.log ("--- Fun Day ---");
+console.log (character + " was in " + place + " on " + time + ".");
+console.log (" Turning around" + character + " saw Joe famous Pizzeria and decided to try a slice " + object + " and " + character +  action + object );
+console.log (" it turned out to be a fun day for " + character);
+
+/// Change values
+
+character = "Dorothy";
+action = "cook";
+object = "tacos";
+place = "apartment";
+time = "late afternoon";
+
+console.log(character + " was at her " + place + " was hungry ");
+console.log("She got hungry so" + character + " decided to " + action + object);
+console.log("it was" + time + " so she only ate one " + object);
